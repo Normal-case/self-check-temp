5056779e8a20205461b91cd820baab983e67b2f9
Normal-case <8282chan94@gmail.com>
2021-10-13 14:40:51 +0900

    change add

--- src/api-server.js
+++ src/api-server.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default class API {
     static imageSend(body){
-        return axios.post("http://192.168.1.54/api/", body)
+        return axios.post("http://192.168.1.54:8000/api/", body)
     }
 }
