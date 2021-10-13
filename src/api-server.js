520ebd5fffcddb52870bd27651f6c0e4cdb407b9
Normal-case <8282chan94@gmail.com>
2021-10-13 14:27:57 +0900

    change ip

--- src/api-server.js
+++ src/api-server.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default class API {
     static imageSend(body){
-        return axios.post("http://192.168.96.1/api/", body)
+        return axios.post("http://192.168.1.54/api/", body)
     }
 }
