d541021c7062aff94430330cb4927024461f8641
Normal-case <8282chan94@gmail.com>
2021-10-13 16:33:48 +0900

    server heroku

--- src/api-server.js
+++ src/api-server.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default class API {
     static imageSend(body){
-        return axios.post("http://192.168.1.54:8000/api/", body)
+        return axios.post("https://plesss2.herokuapp.com//api/", body)
     }
 }
