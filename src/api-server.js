9d594682577978504d3830df31bddc32949c6aa3
root <root@ip-172-31-3-101.ap-northeast-2.compute.internal>
2021-10-14 05:28:18 +0000

    merge

--- src/api-server.js
+++ src/api-server.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default class API {
     static imageSend(body){
-        return axios.post("https://plesss2.herokuapp.com/api/", body)
+        return axios.post("http://13.125.154.101/api/", body)
     }
 }
