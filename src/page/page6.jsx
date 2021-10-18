f9d671382bda50c8baac89348da7bcab072089fe
Normal-case <8282chan94@gmail.com>
2021-10-18 16:09:35 +0900

    height

--- src/page/page6.jsx
+++ src/page/page6.jsx
@@ -106,7 +106,7 @@ const Page6 = () => {
                 const x = canvasRef.current.width * 0.1
                 const y = canvasRef.current.height * 0.4
                 const w = y * 0.2 * 88 / 125
-                const h = height * 0.2
+                const h = canvasRef.current.height * 0.2
 
                 ctx.fillStyle = "white"
 
