7519c3a64bc1969fdf05f10e1b5e3c6da45a1c7c
Normal-case <8282chan94@gmail.com>
2021-10-20 14:41:28 +0900

    link

--- src/page/page5.jsx
+++ src/page/page5.jsx
@@ -13,14 +13,14 @@ const Page5 = () => {
 
             <img src='img/sizeSettings.jpg' style={{margin:'12px auto', width:'90%', border:'1px solid #ddd'}} />
 
-            <h3><span className='red'>셀프 크기 체크는 반드시 모바일 또는 테블릿으로 진행해주세요!</span> <br />크기를 측정하고 싶은 물건을 <br />여권과 함께 책상 또는 바닥에 준비해주세요.</h3>
+            <h3><span className='red'>셀프 크기 체크는 반드시 모바일 또는<br />테블릿으로 진행해주세요!</span> <br />크기를 측정하고 싶은 물건을 <br />여권과 함께 책상 또는 바닥에 준비해주세요.</h3>
 
             <br />
             <br />
 
-            <img src='img/sizeTakePhoto.jgp' style={{margin:'12px auto', width:'90%', border:'1px solid #ddd'}} />
+            <img src='img/sizeTakePhoto.jpg' style={{margin:'12px auto', width:'90%', border:'1px solid #ddd'}} />
 
-            <h3>카메라 화면에 준비된 필터에 맞게 <br />여권을 위치시키고 촬영하기 버튼을 클릭해주세요.<br />화면이 멈추고 크기 체크 시작 버튼이 활성화되면 버튼을 눌러주세요.<br />다시촬영 버튼을 클릭하면 화면이 다시 활성화되고 촬영하실 수 있습니다.</h3>
+            <h3>카메라 화면에 준비된 필터에 맞게 <br />여권을 위치시키고 촬영하기 버튼을 클릭해주세요.<br />화면이 멈추고 크기 체크 시작 버튼이<br />활성화되면 버튼을 눌러주세요.<br />다시촬영 버튼을 클릭하면<br />화면이 다시 활성화되고 촬영하실 수 있습니다.</h3>
 
             <br />
             <br />
@@ -35,6 +35,9 @@ const Page5 = () => {
             <img src='img/sizeResult.jpg' style={{margin: '12px auto', width:'90%', border:'1px solid #ddd'}} />
 
             <h3>체크가 끝나면 위 사진과 같이<br />측정 물체의 길이가 출력됩니다.</h3>
+
+            <br />
+            <br />
 
             <Link to='/page6'><Button variant='outlined'>다음</Button></Link>
 
