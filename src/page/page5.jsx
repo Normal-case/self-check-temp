5eb915323bb79c3b6f1b8e3064317c3fd0b873c9
Normal-case <8282chan94@gmail.com>
2021-10-20 14:34:23 +0900

    page5

--- src/page/page5.jsx
+++ src/page/page5.jsx
@@ -1,148 +1,44 @@
-import React, { useState, useEffect, useRef} from "react"
-import API from '../api-server'
-import { isMobile } from 'react-device-detect'
-import imageCompression from "browser-image-compression"
+import React from "react"
+import { Link } from 'react-router-dom'
+import ArrowBackIcon from '@material-ui/icons/ArrowBack'
 
 const Page5 = () => {
 
-    const [timer, setTimer] = useState(undefined)
-    const [resultFile, setResultFile] = useState(null)
-    const videoRef = useRef(null)
-    const canvasRef = useRef(null)
-    
-    useEffect(() => {
-        if(isMobile){
-            getWebcam((stream => {
-                videoRef.current.srcObject = stream
-            }))
-            
-            startOrStop()
-        }
-        
-    }, [])
-
-    const getWebcam = (callback) => {
-        try {
-            const constraints = {
-                'video': {facingMode: 'environment'},
-                'audio': false
-            }
-            navigator.mediaDevices.getUserMedia(constraints)
-                .then(callback)
-        } catch (error) {
-            console.log(error)
-            return undefined
-        }
-    }
-
-    const startOrStop = () => {
-        if(!timer) {
-            const t = setInterval(() => drawToCanvas(), 0.1)
-            setTimer(t)
-        } else {
-            clearInterval(timer)
-            setTimer(undefined)
-        }
-    }
-
-    const submitSizeAssume = () => {
-        const formData = new FormData()
-        formData.append("base64_img", resultFile)
-        API.sizeSend(formData)
-            .then(resp => console.log(resp))
-            .catch(error => console.log(error))
-    }
-
-    const b64toBlob = (b64Data, contentType='', sliceSize=512) => {
-        const byteCharacters = atob(b64Data)
-        const byteArrays = []
-
-        for (let offset=0;offset<byteCharacters.length;offset+=sliceSize) {
-            const slice = byteCharacters.slice(offset, offset + sliceSize)
-
-            const byteNumbers = new Array(slice.length)
-            for(let i=0;i<slice.length;i++) {
-                byteNumbers[i] = slice.charCodeAt(i)
-            }
-
-            const byteArray = new Uint8Array(byteNumbers)
-            byteArrays.push(byteArray)
-        }
-
-        const blob = new Blob(byteArrays, {type: contentType})
-        return blob
-    }
-
-    const resizeImage = async (targetImage) => {
-        console.log(targetImage)
-        var block = targetImage.split(';')
-        var cType = block[0].split(':')[1]
-        var realData = block[1].split(',')[1]
-        var blob = b64toBlob(realData, cType)
-        const options = {
-            maxWidthOrHeight: 1280
-        }
-
-        try {
-            const compressedFile = await imageCompression(blob, options)
-            setResultFile(compressedFile)
-        }
-        catch (error) { console.log(error) }
-    }
-
-    const drawToCanvas = () => {
-        try {
-            const ctx = canvasRef.current.getContext('2d')
-            canvasRef.current.width = videoRef.current.videoWidth
-            canvasRef.current.height = videoRef.current.videoHeight
-
-            if(ctx && ctx !== null) {
-                if (videoRef.current) {
-                    console.log(videoRef.current)
-                    ctx.drawImage(videoRef.current, 0, 0, canvasRef.current.width, canvasRef.current.height)
-                }
-
-                const half = parseInt(canvasRef.current.height / 2)
-                const blockSize = 7
-
-                ctx.fillStyle = "white"
-                ctx.fillRect((25 - blockSize), half - (62 + blockSize), blockSize, 2 * blockSize)
-                ctx.fillRect(25, half - (62 + blockSize), blockSize, blockSize)
-                
-                ctx.fillRect((113 - blockSize), half - (62 + blockSize), 2 * blockSize, blockSize)
-                ctx.fillRect(113, half - 62, blockSize, blockSize)
-        
-                ctx.fillRect((113 - blockSize), half + 62, 2 * blockSize, blockSize)
-                ctx.fillRect(113, half + (62 - blockSize), blockSize, blockSize)
-        
-                ctx.fillRect((25 - blockSize), half + (62 - blockSize), blockSize, 2 * blockSize)
-                ctx.fillRect(25, half + 62, blockSize, blockSize)
-            }
-
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    const Styles = {
-        Video: {width:"100%"},
-        Canvas: {width:"100%"},
-        None: {display: 'none'},
-        Button: {
-          width: '100px',
-          height: '30px',
-          borderRadius: '4px',
-          backgroundColor: '#FFA500'
-        }
-    }
-
     return(
-        <>
-            <div>
-                <h3>셀프 크기 측정 설명 페이지</h3>
+        <div className='pageWrap arrowWrap'>
+            <Link to="/" className='BackIcon'><ArrowBackIcon fontSize='large' /></Link>
+            <img src='img/exclamation_mark.png' width='48px' />
+            <h3>셀프 크기 측정 설명 페이지</h3>
 
-            </div>
-        </>
+            <img src='img/sizeSettings.jpg' style={{margin:'12px auto', width:'90%', border:'1px solid #ddd'}} />
+
+            <h3><span className='red'>셀프 크기 체크는 반드시 모바일 또는 테블릿으로 진행해주세요!</span> <br />크기를 측정하고 싶은 물건을 <br />여권과 함께 책상 또는 바닥에 준비해주세요.</h3>
+
+            <br />
+            <br />
+
+            <img src='img/sizeTakePhoto.jgp' style={{margin:'12px auto', width:'90%', border:'1px solid #ddd'}} />
+
+            <h3>카메라 화면에 준비된 필터에 맞게 <br />여권을 위치시키고 촬영하기 버튼을 클릭해주세요.<br />화면이 멈추고 크기 체크 시작 버튼이 활성화되면 버튼을 눌러주세요.<br />다시촬영 버튼을 클릭하면 화면이 다시 활성화되고 촬영하실 수 있습니다.</h3>
+
+            <br />
+            <br />
+
+            <img src='img/sizeSpinner.jpg' style={{margin: '12px auto', width:'90%', border:'1px solid #ddd'}} />
+
+            <h3>크기 체크 시작 버튼을 눌렀을 경우<br />위 사진과 같이 크기 측정이 시작됩니다.</h3>
+
+            <br />
+            <br />
+
+            <img src='img/sizeResult.jpg' style={{margin: '12px auto', width:'90%', border:'1px solid #ddd'}} />
+
+            <h3>체크가 끝나면 위 사진과 같이<br />측정 물체의 길이가 출력됩니다.</h3>
+
+            <Link to='/page6'><Button variant='outlined'>다음</Button></Link>
+
+            <br />
+        </div>
     )
 }
 
