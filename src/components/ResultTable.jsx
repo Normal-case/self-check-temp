319ba856c1f9aff384167ee3f3c09b54f753151c
Normal-case <8282chan94@gmail.com>
2021-10-21 13:42:19 +0900

    remove function

--- src/components/ResultTable.jsx
+++ src/components/ResultTable.jsx
@@ -6,7 +6,7 @@ const ResultTable=(clsName, openModal)=> {
         <div className="TableWrap">
         <div className="TableRow">
            <span> <b>{clsName}</b>이(가) 발견되었습니다.</span> 
-           <Button variant="outlined" onClick={openModal(clsName)} style={{border:"1px solid #333", color:"#333"}}> 
+           <Button variant="outlined" style={{border:"1px solid #333", color:"#333"}}> 
 반입규정 확인
            </Button>
         </div>
