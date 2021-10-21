abec5d39eda878e657626c131881999bfbec42d6
Normal-case <8282chan94@gmail.com>
2021-10-21 14:17:20 +0900

    dwekrjwl

--- src/components/ResultTable.jsx
+++ src/components/ResultTable.jsx
@@ -5,7 +5,7 @@ const ResultTable=()=> {
     return (
         <div className="TableWrap">
         <div className="TableRow">
-           <span> <b>{clsName}</b>이(가) 발견되었습니다.</span> 
+           <span> <b>드라이버</b>이(가) 발견되었습니다.</span> 
            <Button variant="outlined" style={{border:"1px solid #333", color:"#333"}}> 
 반입규정 확인
            </Button>
