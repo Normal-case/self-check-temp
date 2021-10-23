42254985851890034556a28b63f262eb6a7e204f
Normal-case <8282chan94@gmail.com>
2021-10-23 19:35:16 +0900

    erqe

--- src/page/page6.jsx
+++ src/page/page6.jsx
@@ -11,7 +11,6 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
 
