874903d9220b6af27554b14ff678567ba52c4a4d
Normal-case <8282chan94@gmail.com>
2021-10-24 18:20:57 +0900

    for loop

--- src/page/admin.jsx
+++ src/page/admin.jsx
@@ -15,8 +15,37 @@ const Admin = () => {
   const getResponse = (resp) => {
     console.log(resp)
     setResponseResult(resp)
-    console.log(resp['data'])
-    console.log(resp['data']['knife'])
+  }
+
+  const forLoopKnife = () => {
+    const result = []
+    for (let i=0;i<responseResult['data']['knife'];i++){
+      result.push(
+        <>
+        {responseResult['data']['knife'][i]['fields']['product']}
+        <table>
+          <thead>
+            <tr>
+              <th>위탁수하물</th>
+              <th>기내휴대</th>
+              <th>특별규정</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{responseResult['data']['knife'][i]['fields']['checked']}</td>
+              <td>{responseResult['data']['knife'][i]['fields']['carryon']}</td>
+              <td>{responseResult['data']['knife'][i]['fields']['special']}</td>
+            </tr>
+            <tr>
+              <td colSpan='3'>{responseResult['data']['knife'][i]['fields']['description']}</td>
+            </tr>
+          </tbody>
+        </table>
+        </>
+      )
+    }
+    return result
   }
 
   return (
@@ -24,35 +53,7 @@ const Admin = () => {
       <h3>관리자 페이지</h3>
       <div>
         칼
-        {/* { responseResult['data']['knife'] ?
-          responseResult['data']['knife'].map((value, index) => 
-            <>
-            <h4>{value['fields']['product']}</h4>
-            <table>
-              <thead>
-                <tr>
-                  <th>위탁수하물</th>
-                  <th>기내휴대</th>
-                  <th>특별규정</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>{value['fields']['checked']}</td>
-                  <td>{value['fields']['carryon']}</td>
-                  <td>{value['fields']['special']}</td>
-                </tr>
-                <tr>
-                  <td colSpan='3'>{value['fields']['description']}</td>
-                </tr>
-              </tbody>
-            </table>
-            </>
-          ) : null
-        } */}
-        { responseResult['data']['knife'] 
-        ? responseResult['data']['knife'].map((value, index) => <h4>{value['fields']['product']}</h4>) : null
-        }
+        {forLoopKnife()}
       </div>
      </>  
   );
