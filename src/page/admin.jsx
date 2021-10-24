da6dc7424cd7a6bd2644d416bd5d40f7b0eea375
Normal-case <8282chan94@gmail.com>
2021-10-24 18:22:52 +0900

    reqrewR

--- src/page/admin.jsx
+++ src/page/admin.jsx
@@ -15,45 +15,46 @@ const Admin = () => {
   const getResponse = (resp) => {
     console.log(resp)
     setResponseResult(resp)
+    console.log(resp['data']['knife'])
   }
 
-  const forLoopKnife = () => {
-    const result = []
-    for (let i=0;i<responseResult['data']['knife'];i++){
-      result.push(
-        <>
-        {responseResult['data']['knife'][i]['fields']['product']}
-        <table>
-          <thead>
-            <tr>
-              <th>위탁수하물</th>
-              <th>기내휴대</th>
-              <th>특별규정</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{responseResult['data']['knife'][i]['fields']['checked']}</td>
-              <td>{responseResult['data']['knife'][i]['fields']['carryon']}</td>
-              <td>{responseResult['data']['knife'][i]['fields']['special']}</td>
-            </tr>
-            <tr>
-              <td colSpan='3'>{responseResult['data']['knife'][i]['fields']['description']}</td>
-            </tr>
-          </tbody>
-        </table>
-        </>
-      )
-    }
-    return result
-  }
+  // const forLoopKnife = () => {
+  //   const result = []
+  //   for (let i=0;i<responseResult['data']['knife'];i++){
+  //     result.push(
+  //       <>
+  //       {responseResult['data']['knife'][i]['fields']['product']}
+  //       <table>
+  //         <thead>
+  //           <tr>
+  //             <th>위탁수하물</th>
+  //             <th>기내휴대</th>
+  //             <th>특별규정</th>
+  //           </tr>
+  //         </thead>
+  //         <tbody>
+  //           <tr>
+  //             <td>{responseResult['data']['knife'][i]['fields']['checked']}</td>
+  //             <td>{responseResult['data']['knife'][i]['fields']['carryon']}</td>
+  //             <td>{responseResult['data']['knife'][i]['fields']['special']}</td>
+  //           </tr>
+  //           <tr>
+  //             <td colSpan='3'>{responseResult['data']['knife'][i]['fields']['description']}</td>
+  //           </tr>
+  //         </tbody>
+  //       </table>
+  //       </>
+  //     )
+  //   }
+  //   return result
+  // }
 
   return (
      <>
       <h3>관리자 페이지</h3>
       <div>
         칼
-        {forLoopKnife()}
+        {/* {forLoopKnife()} */}
       </div>
      </>  
   );
