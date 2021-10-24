6623d676fba605412b68c8638966ff45c8def8b0
Normal-case <8282chan94@gmail.com>
2021-10-25 01:12:59 +0900

    change img

--- src/page/page2.jsx
+++ src/page/page2.jsx
@@ -10,7 +10,7 @@ const page2 = () => {
       <img src="img/exclamation_mark.png" width="48px" />
       <h2>셀프 체크 설명서</h2>
 
-<img src="img/guide1.png" style={{margin:'12px auto', width:"90%"}} />
+<img src="img/checkSample.jpg" style={{margin:'12px auto', width:"90%"}} />
 
 <h3>먼저, 항공기에 반입 가능한지 <br /> 확인하고 싶은 물품들을 <br /> 책상 또는 바닥에 잘 나열해주세요.</h3>
 
@@ -18,7 +18,7 @@ const page2 = () => {
 <br />
 <br />
 
-<img src="img/guide2.png" style={{margin:'12px auto', width:"90%", border:'1px solid #ddd'}} />
+<img src="img/checkUpload.jpg" style={{margin:'12px auto', width:"90%", border:'1px solid #ddd'}} />
 <h3>사진을 찍은 후, <br /> 셀프 체크 화면에 사진을 업로드 해주세요.<br /> 
 셀프 체크 시작버튼이 활성화 되면 <br /> 해당 버튼을 눌러 시작해주세요.
 </h3>
@@ -26,13 +26,15 @@ const page2 = () => {
 <br />
 <br />
 
-<img src="img/guide3.png" style={{margin:'12px auto', width:"90%", border:'1px solid #ddd'}} />
-<h3>검사 결과와 함께 검출된 물품의 <br /> 항공 규정을 확인할 수 있습니다. <br /> <br /> 
-금지 물품은 <span className="red">빨간색</span> , <br /> 기내&위탁 가능 물품은 <span className="green">초록색</span> , <br /> 크기&용량 제한 물품은 <span className="yellow">노란색</span>으로 표기됩니다.</h3>
+<img src="img/checkResult.png" style={{margin:'12px auto', width:"90%", border:'1px solid #ddd'}} />
+<h3>검사 결과와 함께 검출된 물품의 <br /> 항공 규정을 확인할 수 있습니다.</h3>
 
 
 <br />
 <br />
+
+<img src="img/checkModal.jpg" style={{margin:'12px auto', width:'90%', border:'1px solid #ddd'}} />
+<h3>규정을 통해 보다 자세한 내용을<br />확인할 수 있습니다.</h3>
 
 <footer>
 <Link to="/page3"> <Button variant="outlined">다음</Button> </Link>
