fd8376f12fc0f3aff2e2c335831baeca557ff6da
Normal-case <8282chan94@gmail.com>
2021-10-26 14:22:39 +0900

    change server

--- src/api-server.js
+++ src/api-server.js
@@ -2,15 +2,15 @@ import axios from "axios";
 
 export default class API {
     static imageSend(body){
-        return axios.post("https://self-check.site/api/", body)
+        return axios.post("https://self-check-api.shop/api/", body)
     }
 
     static sizeSend(body){
-        return axios.post("https://self-check.site/api/size/", body)
+        return axios.post("https://self-check-api.shop/api/size/", body)
     }
 
     static volumnSend(body){
-        return axios.post("https://self-check.site/api/volumn/", body)
+        return axios.post("https://self-check-api.shop/api/volumn/", body)
     }
 
     static adminSend(){
