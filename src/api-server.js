7fda36ce199fe7cb6c7e518dd81a42a450d21037
Normal-case <8282chan94@gmail.com>
2022-02-24 17:24:30 +0900

    change destination

--- src/api-server.js
+++ src/api-server.js
@@ -1,23 +1,23 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default class API {
-    static imageSend(body){
-        return axios.post("https://self-check-api.shop/api/", body)
-    }
+  static imageSend(body) {
+    return axios.post('https://self-check-api.shop:8001/api/object', body);
+  }
 
-    static ocrSend(body){
-        return axios.post("https://self-check-api.shop/api/ocr/", body)
-    }
+  static ocrSend(body) {
+    return axios.post('https://self-check-api.shop:8001/api/ocr', body);
+  }
 
-    static sizeSend(body){
-        return axios.post("https://self-check-api.shop/api/size/", body)
-    }
+  static sizeSend(body) {
+    return axios.post('https://self-check-api.shop:8001/api/size', body);
+  }
 
-    static volumnSend(body){
-        return axios.post("https://self-check-api.shop/api/volumn/", body)
-    }
+  static volumnSend(body) {
+    return axios.post('https://self-check-api.shop:8001/api/volumn', body);
+  }
 
-    static adminSend(){
-        return axios.get("https://self-check-api.shop/api/admin/")
-    }
+  static adminSend() {
+    return axios.get('https://self-check-api.shop/api/admin/');
+  }
 }
