843bf20c81fbfb3ca1b35b088e7054fadbf6698d
Normal-case <8282chan94@gmail.com>
2022-02-25 08:16:48 +0900

    remove admin and ocr button

--- src/components/TabNav.jsx
+++ src/components/TabNav.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import { Button, Tabs, Tab } from '@mui/material';
 
 interface TabPanelProps {
@@ -45,62 +45,99 @@ export default function TabNav() {
   };
 
   const callAdmin = () => {
-    window.location.href = 'https://self-check-api.shop/admin/api/prohibit/'
-  }
+    window.location.href = 'https://self-check-api.shop/admin/api/prohibit/';
+  };
 
   return (
-        <div className="pageWrap firstPageWrap">
-    <img src="img/plane.svg" className="plane" />
-  
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="셀프 체크" {...a11yProps(0)} style={{width:20, color:'#001189'}} />
-          <Tab label="크기 측정" {...a11yProps(1)} style={{width:20, color:'#001189'}} />
-          <Tab label="부피 측정" {...a11yProps(0)} style={{width:20, color:'#001189'}} />
-        </Tabs>
+    <div className="pageWrap firstPageWrap">
+      <img src="img/plane.svg" className="plane" />
+
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+          >
+            <Tab
+              label="셀프 체크"
+              {...a11yProps(0)}
+              style={{ width: 20, color: '#001189' }}
+            />
+            <Tab
+              label="크기 측정"
+              {...a11yProps(1)}
+              style={{ width: 20, color: '#001189' }}
+            />
+            <Tab
+              label="부피 측정"
+              {...a11yProps(0)}
+              style={{ width: 20, color: '#001189' }}
+            />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+          <img src="img/cont1.svg" className="ImgCont" width="60" />
+          <span className="MainText">
+            기내 반입금지 물품, 집에서는 체크할 수 없을까요? <br />
+            가능합니다! 항공보안 인공지능을 이용해보세요. <br /> 사진을 찍을 수
+            있는 <span className="textImportant">핸드폰</span>만 있으면 됩니다.
+          </span>
+
+          <br />
+          <br />
+          <br />
+          <Link to="/page3">
+            {' '}
+            <Button variant="outlined">셀프체크 시작하기</Button>{' '}
+          </Link>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <img src="img/cont2.svg" className="ImgCont" width="100" />
+          <span className="MainText">
+            꼭 가져가야 하는 물건이 있는데, <br />
+            반입금지 규정은 너무 많고 복잡한가요? <br />
+            디테일한 감정을 위해 크기 측정을 해보세요! <br />
+            항공보안 인공지능이 함께합니다. <br />{' '}
+            <span className="textImportant">여권, 혹은 A4용지</span>를
+            준비해주세요.
+          </span>
+
+          <br />
+          <br />
+          <br />
+          <Link to="/page5">
+            {' '}
+            <Button variant="outlined">크기측정 시작하기</Button>{' '}
+          </Link>
+        </TabPanel>
+
+        <TabPanel value={value} index={2}>
+          <img src="img/cont3.svg" className="ImgCont" width="80" />
+          <span className="MainText">
+            항공기에 물을 가지고 들어가면 <br />
+            안된다는 사실, 아셨나요?
+            <br />
+            승객 여행의 안전을 위해 식별되지 않은 <br />
+            <span className="textImportant">
+              일정량 이상의 액체는 기내 반입이 금지
+            </span>
+            됩니다.
+            <br />
+            기내 반입 전, 병의 부피를 측정해봅시다.
+          </span>
+
+          <br />
+          <br />
+          <br />
+          <Link to="/page7">
+            {' '}
+            <Button variant="outlined">부피측정 시작하기</Button>{' '}
+          </Link>
+        </TabPanel>
       </Box>
-      <TabPanel value={value} index={0}>
-        <img src="img/cont1.svg" className="ImgCont" width="60" />
-     <span className="MainText">
-기내 반입금지 물품, 집에서는 체크할 수 없을까요? <br />
-가능합니다! 항공보안 인공지능을 이용해보세요. <br /> 사진을 찍을 수 있는 <span className="textImportant">핸드폰</span>만 있으면 됩니다.
-     </span>
 
-<br /><br /><br />
- <Link to="/page3"> <Button variant="outlined">셀프체크 시작하기</Button> </Link>
-
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-     <img src="img/cont2.svg" className="ImgCont" width="100" />
-  <span className="MainText">
-꼭 가져가야 하는 물건이 있는데, <br />반입금지 규정은 너무 많고 복잡한가요? <br />
-디테일한 감정을 위해 크기 측정을 해보세요! <br />항공보안 인공지능이 함께합니다. <br /> <span className="textImportant">여권, 혹은 A4용지</span>를 준비해주세요.
-     </span>
-
-<br /><br /><br />
-<Link to="/page5"> <Button variant="outlined">크기측정 시작하기</Button> </Link>
-    </TabPanel>
-
-
- <TabPanel value={value} index={2}>
-     <img src="img/cont3.svg" className="ImgCont" width="80" />
-  <span className="MainText">
-항공기에 물을 가지고 들어가면 <br />
-안된다는 사실, 아셨나요?<br />
-승객 여행의 안전을 위해 식별되지 않은 <br /> 
-<span className="textImportant">일정량 이상의 액체는 기내 반입이 금지</span>됩니다.<br />
-기내 반입 전, 병의 부피를 측정해봅시다.
-     </span>
-
-<br /><br /><br />
-<Link to="/page7"> <Button variant="outlined">부피측정 시작하기</Button> </Link>
-
-    </TabPanel>
-
-    </Box>
-
-    <span className='adminLink' onClick={callAdmin}>관리자 페이지</span>
-      </div>
+      {/* <span className='adminLink' onClick={callAdmin}>관리자 페이지</span> */}
+    </div>
   );
 }
--- src/page/page4.jsx
+++ src/page/page4.jsx
@@ -1,212 +1,294 @@
-import React, { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
-import { Button } from '@mui/material'
-import imageCompression from 'browser-image-compression'
-import ArrowBackIcon from '@material-ui/icons/ArrowBack'
-import API from '../api-server'
-import Loader from "react-loader-spinner"
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
+import imageCompression from 'browser-image-compression';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import API from '../api-server';
+import Loader from 'react-loader-spinner';
 
-import CupDesc from './modal/cup'
-import BottleDesc from './modal/bottle'
-import DriverDesc from './modal/driver'
-import KnifeDesc from './modal/knife'
-import LighterDesc from './modal/lighter'
-import ScissorsDesc from './modal/scissors'
-import SprayDesc from './modal/spray'
-import FoodDesc from './modal/food'
-import Modal from './modal'
+import CupDesc from './modal/cup';
+import BottleDesc from './modal/bottle';
+import DriverDesc from './modal/driver';
+import KnifeDesc from './modal/knife';
+import LighterDesc from './modal/lighter';
+import ScissorsDesc from './modal/scissors';
+import SprayDesc from './modal/spray';
+import FoodDesc from './modal/food';
+import Modal from './modal';
 
 // upload image component and display result component for self check
 const Page4 = () => {
-
-  const [selectedFile, setSelectedFile] = useState()                // image file that will be sent to the server
-  const [imgBase64, setImgBase64] = useState()                      // image file that display on the page
-  const [uploaded, setUploaded] = useState(false)                   // uploaded => custom image uploaded, !uploaded => default image uploaded
-  const [spinner, setSpinner] = useState(false)                     // spinner => display spinner, !spinner => hide spinner
-  const [resultResponse, setResultResponse] = useState(null)        // save information received from the server
-  const [pageName, setPageName] = useState('uploadPage')            // pageName == uploadPage ? upload page : result page
-  const [labelResult, setLabelResult] = useState(null)              // save labelResult Object
-  const [modalVisible, setModalVisible] = useState(false)           // modal display variable
-  const [modalCase, setModalCase] = useState('')                    // product name
+  const [selectedFile, setSelectedFile] = useState(); // image file that will be sent to the server
+  const [imgBase64, setImgBase64] = useState(); // image file that display on the page
+  const [uploaded, setUploaded] = useState(false); // uploaded => custom image uploaded, !uploaded => default image uploaded
+  const [spinner, setSpinner] = useState(false); // spinner => display spinner, !spinner => hide spinner
+  const [resultResponse, setResultResponse] = useState(null); // save information received from the server
+  const [pageName, setPageName] = useState('uploadPage'); // pageName == uploadPage ? upload page : result page
+  const [labelResult, setLabelResult] = useState(null); // save labelResult Object
+  const [modalVisible, setModalVisible] = useState(false); // modal display variable
+  const [modalCase, setModalCase] = useState(''); // product name
 
   // label change english name to korean name
   const labelNameChange = {
-    'cup': '컵',
-    'bottle': '병',
-    'knife': '칼',
-    'scissors': '가위',
-    'spray': '스프레이',
-    'lighter': '라이터',
-    'screwdriver': '드라이버',
-    'food': '음식물'
-  }
+    cup: '컵',
+    bottle: '병',
+    knife: '칼',
+    scissors: '가위',
+    spray: '스프레이',
+    lighter: '라이터',
+    screwdriver: '드라이버',
+    food: '음식물',
+  };
 
-  useEffect(() => {}, [spinner])
-
+  useEffect(() => {}, [spinner]);
 
   // resize uploaded image before sending server
   const resizeImage = async (targetImage) => {
     const options = {
-      maxWidthOrHeight: 1280
-    }
+      maxWidthOrHeight: 1280,
+    };
 
     try {
       const compressedFile = await imageCompression(targetImage, options);
-      setSelectedFile(compressedFile)
-      handleChangeFile(compressedFile)
+      setSelectedFile(compressedFile);
+      handleChangeFile(compressedFile);
+    } catch (error) {
+      console.log(error);
     }
-    catch (error) {
-      console.log(error)
-    }
-  }
+  };
 
   // preview uploaded image function
   const handleChangeFile = (compressedFile) => {
     let reader = new FileReader();
     reader.onloadend = () => {
       const base64 = reader.result;
-      if(base64) {
+      if (base64) {
         setImgBase64(base64.toString());
       }
-    }
+    };
     if (compressedFile) {
       reader.readAsDataURL(compressedFile);
-      setUploaded(true)
+      setUploaded(true);
     }
-  }
+  };
 
   // send image file to server when the self-check button click
   const submitImage = () => {
-    const formData = new FormData()
-    formData.append('img', selectedFile)
-    setSpinner(true)
+    const formData = new FormData();
+    formData.append('img', selectedFile);
+    setSpinner(true);
     API.imageSend(formData)
-      .then(resp => getResponse(resp))
-      .catch(error => console.log(error))
-  }
+      .then((resp) => getResponse(resp))
+      .catch((error) => console.log(error));
+  };
 
   // send image file to server for getting ocr information
   const ocrCheck = () => {
-    const formData = new FormData()
-    formData.append('img', selectedFile)
-    setSpinner(true)
+    const formData = new FormData();
+    formData.append('img', selectedFile);
+    setSpinner(true);
     API.ocrSend(formData)
-      .then(resp => getResponse(resp))
-      .catch(error => console.log(error))
-  }
+      .then((resp) => getResponse(resp))
+      .catch((error) => console.log(error));
+  };
 
   // save response
   // resp => {'data': {'after_detection':'base64 img', 'label':'detected label'}}
   const getResponse = (resp) => {
-    setResultResponse(resp)
-    setSpinner(false)
-    setPageName('resultPage')
-    const label_result = []
-    for(let i=0;i<resp['data']['label'].length;i++){
-      const labName = labelNameChange[resp['data']['label'][i]]
-      if(label_result[labName] !== undefined){
-        label_result[labName] += 1
+    setResultResponse(resp);
+    setSpinner(false);
+    setPageName('resultPage');
+    const label_result = [];
+    for (let i = 0; i < resp['data']['label'].length; i++) {
+      const labName = labelNameChange[resp['data']['label'][i]];
+      if (label_result[labName] !== undefined) {
+        label_result[labName] += 1;
       } else {
-        label_result[labName] = 1
+        label_result[labName] = 1;
       }
     }
-    setLabelResult(label_result)
-  }
+    setLabelResult(label_result);
+  };
 
   // open modal
-  const openModal = product => {
-    setModalVisible(true)
-    setModalCase(product)
-  }
+  const openModal = (product) => {
+    setModalVisible(true);
+    setModalCase(product);
+  };
 
   // close modal
   const closeModal = () => {
-    setModalVisible(false)
-  }
+    setModalVisible(false);
+  };
 
   // reload web page
   const retry = () => {
-    window.location.reload()
-  }
+    window.location.reload();
+  };
 
   // choose the right modal
   const chooseModal = () => {
-    switch(modalCase) {
+    switch (modalCase) {
       case '컵':
-        return <CupDesc />
+        return <CupDesc />;
       case '병':
-        return <BottleDesc />
+        return <BottleDesc />;
       case '칼':
-        return <KnifeDesc />
+        return <KnifeDesc />;
       case '가위':
-        return <ScissorsDesc />
+        return <ScissorsDesc />;
       case '스프레이':
-        return <SprayDesc />
+        return <SprayDesc />;
       case '라이터':
-        return <LighterDesc />
+        return <LighterDesc />;
       case '드라이버':
-        return <DriverDesc />
+        return <DriverDesc />;
       case '음식물':
-        return <FoodDesc />
+        return <FoodDesc />;
       default:
-        return
+        return;
     }
-  }
+  };
 
   return (
-    <div className='pageWrap arrowWrap'>
-      <Link to="/page3" className="BackIcon"><ArrowBackIcon fontSize="large" /></Link>
-      { pageName === 'uploadPage' ? 
+    <div className="pageWrap arrowWrap">
+      <Link to="/page3" className="BackIcon">
+        <ArrowBackIcon fontSize="large" />
+      </Link>
+      {pageName === 'uploadPage' ? (
         <div>
-
-          { !uploaded ? <div className="downloadWrap"> <img src='img/download.png' width="100px" /> </div> : <div className="downloadWrap downloadClearWrap"> <img src={imgBase64} width="80px" /> </div>  }
-          <label htmlFor='input-file' id='inputFileLabel'>사진 업로드</label>
-          <input type='file' id='input-file' style={{display:'none'}} onChange={(e) => resizeImage(e.target.files[0])} />
-
+          {!uploaded ? (
+            <div className="downloadWrap">
+              {' '}
+              <img src="img/download.png" width="100px" />{' '}
+            </div>
+          ) : (
+            <div className="downloadWrap downloadClearWrap">
+              {' '}
+              <img src={imgBase64} width="80px" />{' '}
+            </div>
+          )}
+          <label htmlFor="input-file" id="inputFileLabel">
+            사진 업로드
+          </label>
+          <input
+            type="file"
+            id="input-file"
+            style={{ display: 'none' }}
+            onChange={(e) => resizeImage(e.target.files[0])}
+          />
           <ul className="caution_ul">
-            <li>➤  <b>무늬가 없는 배경</b>에서 촬영해주세요.</li>
-            <li>➤  <b>물품 전체</b>가 나오도록 촬영해주세요.</li>
-            <li>➤  <b>물품이 겹치지 않게</b> 촬영해주세요.</li>
+            <li>
+              ➤ <b>무늬가 없는 배경</b>에서 촬영해주세요.
+            </li>
+            <li>
+              ➤ <b>물품 전체</b>가 나오도록 촬영해주세요.
+            </li>
+            <li>
+              ➤ <b>물품이 겹치지 않게</b> 촬영해주세요.
+            </li>
           </ul>
-
-          <button className="selfCheckBtn" disabled={!uploaded} onClick={submitImage}>셀프 체크 시작!</button> <br /><br />
-          <span className='adminLink' onClick={ocrCheck}>OCR 추가</span>
-          { spinner ? <div className='modal'>
-            <div className='spinnerModal'>
-              <Loader type="Circles" color="#00BFFF" height={100} width={100} /> <br />
-              <span className="selfCheckDelayText">셀프 체크 중입니다...</span>
+          <button
+            className="selfCheckBtn"
+            disabled={!uploaded}
+            onClick={submitImage}
+          >
+            셀프 체크 시작!
+          </button>{' '}
+          <br />
+          <br />
+          {/* <span className='adminLink' onClick={ocrCheck}>OCR 추가</span> */}
+          {spinner ? (
+            <div className="modal">
+              <div className="spinnerModal">
+                <Loader
+                  type="Circles"
+                  color="#00BFFF"
+                  height={100}
+                  width={100}
+                />{' '}
+                <br />
+                <span className="selfCheckDelayText">
+                  셀프 체크 중입니다...
+                </span>
+              </div>
             </div>
-          </div> : null}
+          ) : null}
         </div>
-         : <div className="SelfWrap">
-            <h3>➤ 셀프체크 결과입니다.</h3>
-            <img src={'data:image/gif;base64,' + resultResponse['data']['after_detection']} alt='' />
-            <div className='TableWrap'>
-            {labelResult ? <div><h3>발견된 물품</h3>{Object.keys(labelResult).map((key) => (
-            <div className='TableRow'>
-              <span><b>{key}</b></span>
-              <Button onClick={() => {openModal(key)}} variant="outlined" style={{border:"1px solid #333", color:"#333"}}> 
-                반입규정 확인
+      ) : (
+        <div className="SelfWrap">
+          <h3>➤ 셀프체크 결과입니다.</h3>
+          <img
+            src={
+              'data:image/gif;base64,' +
+              resultResponse['data']['after_detection']
+            }
+            alt=""
+          />
+          <div className="TableWrap">
+            {labelResult ? (
+              <div>
+                <h3>발견된 물품</h3>
+                {Object.keys(labelResult).map((key) => (
+                  <div className="TableRow">
+                    <span>
+                      <b>{key}</b>
+                    </span>
+                    <Button
+                      onClick={() => {
+                        openModal(key);
+                      }}
+                      variant="outlined"
+                      style={{ border: '1px solid #333', color: '#333' }}
+                    >
+                      반입규정 확인
+                    </Button>
+                  </div>
+                ))}
+              </div>
+            ) : null}
+          </div>
+          <div className="ButtonWrap">
+            <Button
+              onClick={() => {
+                retry();
+              }}
+              varient="outlined"
+              style={{
+                border: '1px solid #333',
+                color: '#333',
+                borderRadius: '5px',
+              }}
+            >
+              다시하기
+            </Button>
+            &nbsp;&nbsp;
+            <Link to="/">
+              <Button
+                varient="outlined"
+                style={{
+                  border: '1px solid #333',
+                  color: '#333',
+                  borderRadius: '5px',
+                }}
+              >
+                처음으로
               </Button>
-            
-            </div>))}</div> : null}
-    
-            </div>
-            <div className='ButtonWrap'>
-              <Button onClick={() => {retry()}} varient='outlined' style={{border:'1px solid #333', color:'#333', borderRadius:'5px'}}>다시하기</Button>&nbsp;&nbsp;<Link to='/'><Button varient='outlined' style={{border:'1px solid #333', color:'#333', borderRadius:'5px'}}>처음으로</Button></Link>
-            </div>
-           </div>
-      }
+            </Link>
+          </div>
+        </div>
+      )}
 
-      {
-        modalVisible && <Modal
-        visible={modalVisible}
-        closable={true}
-        maskClosable={true}
-        onClose={closeModal}>
-          { chooseModal() }
+      {modalVisible && (
+        <Modal
+          visible={modalVisible}
+          closable={true}
+          maskClosable={true}
+          onClose={closeModal}
+        >
+          {chooseModal()}
         </Modal>
-      }
+      )}
     </div>
   );
 };
